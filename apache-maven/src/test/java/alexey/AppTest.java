a6fb5740f2db13c3c9c408e275f5bc673a543add
AlexeyAleks <AlexeyAleks@mail.ru>
2020-07-12 11:38:15 +0300

    solution tasks01-04

--- apache-maven/src/test/java/alexey/AppTest.java
+++ apache-maven/src/test/java/alexey/AppTest.java
@@ -1,8 +1,8 @@
 package alexey;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-import static junit.framework.TestCase.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class AppTest {
     @Test
