d66b7616ec26ce2b10b93be141cb2174cd1e6205
AlexeyAleks <AlexeyAleks@mail.ru>
2020-07-12 12:44:41 +0300

    solution tasks01-03

--- apache-maven-profiles/src/main/java/alexey/Calculator.java
+++ apache-maven-profiles/src/main/java/alexey/Calculator.java
@@ -1,19 +1,19 @@
 package alexey;
 
-public class Calculator {
-    int add(int x, int y) {
+public final class Calculator {
+    int add(final int x, final int y) {
         return x + y;
     }
 
-    int subtract(int x, int y) {
+    int subtract(final int x, final int y) {
         return x - y;
     }
 
-    int multiply(int x, int y) {
+    int multiply(final int x, final int y) {
         return x * y;
     }
 
-    int divide(int x, int y) {
+    int divide(final int x, final int y) {
         return x / y;
     }
 }
